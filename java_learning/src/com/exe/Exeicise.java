f34ed23b41ddcc89c2b3ad2d116024d761c2a491
evans <87635415@qq.com>
2020-08-20 23:24:14 +0800

    add java_learning project

---
+++ java_learning/src/com/exe/Exeicise.java
@@ -0,0 +1,9 @@
+package com.exe;
+
+public class Exeicise {
+    public static void print() {
+        System.out.println(time);
+    }
+
+    public static int time = 1;
+}
---
+++ java_learning/src/com/exe/Test.java
@@ -0,0 +1,33 @@
+package com.exe;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class Test {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        int month = date.getMonthValue();
+        int today = date.getDayOfMonth();
+
+        date = date.minusDays(today - 1);
+        DayOfWeek weekday = date.getDayOfWeek();
+        int value = weekday.getValue();
+
+        System.out.println("Mon Tue Wed Thu Fri Sat Sun");
+        for (int i = 1; i < value; ++i) {
+            System.out.print("    ");
+        }
+        while (date.getMonthValue() == month) {
+            System.out.printf("%3d", date.getDayOfMonth());
+            if (today == date.getDayOfMonth()) {
+                System.out.print("*");
+            } else {
+                System.out.print(" ");
+            }
+            date = date.plusDays(1);
+            if (date.getDayOfWeek().getValue() == 1) {
+                System.out.println();
+            }
+        }
+    }
+}
